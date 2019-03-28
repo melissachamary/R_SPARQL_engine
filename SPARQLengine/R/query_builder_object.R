b264f4711a475e8c79aaabbdbea19314b0cb616e
Melissa <melissa.cha.mary@gmail.com>
2019-03-28 06:34:49 +0100

    Transformation into R Library

--- SPARQLengine/R/query_builder_object.R
+++ SPARQLengine/R/query_builder_object.R
@@ -1,4 +1,7 @@
-
+#' @title SPARQL_query_builder
+#' class enable to make some simple and parametric SPARQL queries
+#' @export
+#'
 setClass("SPARQL_query_builder", representation=
          representation(prefix="data.frame",graph="list",method="character",constraint="list"),
          prototype = prototype(
@@ -10,14 +13,21 @@ setClass("SPARQL_query_builder", representation=
          )
 )
 #### Prefix slot setter, getter
+#' @title getPrefix
+#' @return prefix data.frame for the query
+#' @export
 setGeneric(name="getPrefix",
            def=function(object){
              standardGeneric("getPrefix")
            })
+
 setMethod(f="getPrefix",signature="SPARQL_query_builder",function(object){
   return(object@prefix)
 })
 
+#' @title addPrefix WARNING : unimplemented
+#' @return import prefix into the class
+#' @export
 setGeneric(name="addPrefix",
            def=function(object,prefix_table){
              standardGeneric("addPrefix")
@@ -31,6 +41,9 @@ setMethod("addPrefix","SPARQL_query_builder",function(object,prefix_table){
   }
 })
 
+#' @title getGraph
+#' @return graph list
+#' @export
 setGeneric(name="getGraph",
            def=function(object){
              standardGeneric("getGraph")
@@ -38,7 +51,9 @@ setGeneric(name="getGraph",
 setMethod(f="getGraph",signature="SPARQL_query_builder",function(object){
   return(object@graph)
 })
-
+#' @title getMethod
+#' @return return method (select, insert etc.)
+#' @export
 setGeneric(name="getMethod",
            def=function(object){
              standardGeneric("getMethod")
@@ -47,6 +62,9 @@ setMethod(f="getMethod",signature="SPARQL_query_builder",function(object){
   return(object@method)
 })
 
+#' @title getConstraint
+#' @return list of constraint  that correspond to constaint in SPARQL query (triple pattern)
+#' @export
 setGeneric(name="getConstraint",
            def=function(object){
              standardGeneric("getConstraint")
@@ -54,9 +72,21 @@ setGeneric(name="getConstraint",
 setMethod(f="getConstraint",signature="SPARQL_query_builder",function(object){
   return(object@constraint)
 })
+#' @title getParameterConstraint
+#' @return list of parameter that should be filled before the query execution (parameter should looks like "<<[:alpha:]+>>")
+#' @export
+setGeneric(name="getParameterConstraint",
+           def=function(object){
+             standardGeneric("getParameterConstraint")
+           })
+setMethod(f="getParameterConstraint",signature="SPARQL_query_builder",function(object){
+  return(object@constraint$parameters)
+})
 
-
-
+#' @title addConstraint
+#' @description add constraint by 1. checking element type and refine parameter element list (if one of the element is parameter). Notice that parameters are identified by the following pattern "<<[:alpha:]+>>"
+## @input subj, pred, obj : element corresponding to query element (?xxx) explicit/prefixed uri or parameter
+#' @export
 setGeneric(name="addConstraint",
            def=function(object,subj, pred, obj){
              standardGeneric("addConstraint")
@@ -72,6 +102,62 @@ setMethod(f="addConstraint",signature="SPARQL_query_builder",function(object, su
 object
 })
 
+#' @title addConstraintFrame
+#' @description add set of constraint in the object
+## @input dataFrame : data.frame with the same column name ad the object constraint data frame
+## @input keep : elements that should be return by the query, if keep is empty all query eement will be return (?XXX)
+#' @export
+setGeneric(name="addConstraintFrame",
+           def=function(object, data, keep){
+             standardGeneric("addConstraintFrame")
+           })
+setMethod(f="addConstraintFrame",signature="SPARQL_query_builder",function(object, data, keep){
+
+  if(is.data.frame(data) && nrow(data)>0 && length(which(c("subj","pred","obj")%in%colnames(data)))==3){
+    cst<-getConstraint(object)
+    cst$constraintDescription<-rbind(cst$constraintDescription,
+                                     data.frame(typeConstraint="simple",  groupConstraint=NA,
+                                                string = unlist(apply(X=data[,c("subj","pred","obj")],1,
+                                                                       FUN = function(X){
+                                                                         paste(X, collapse=" ")
+                                                                       }))
+                                                  ))
+    queryKeep<-keep[isQueryVariable(keep)]
+    if(is.null(keep)||is.na(keep)){
+      element<-character()
+      element<-c(element,as.character(data$subj[which(isQueryVariable(data$subj))]),
+                 as.character(data$obj[which(isQueryVariable(data$obj))]),
+                 as.character(data$pred[which(isQueryVariable(data$pred))])
+      )
+      print(data$subj[which(isQueryVariable(data$subj))])
+      cst$element<-unique(c(cst$element,element))
+    }else if(length(which(queryKeep %in% unlist(as.vector(data))))==length(queryKeep)){
+      cst$element<-unique(c(cst$element,unique(queryKeep)))
+    }else{
+      print("query keep issue")
+      #print(queryKeep)
+      print(keep)
+      #print(unlist(as.vector(data)))
+      print(queryKeep[which(!queryKeep %in% unlist(as.vector(data)))])
+    }
+    parameter<-character()
+    parameter<-c(parameter,as.character(data$subj[which(isQueryParameter(data$subj))]),
+                 as.character(data$obj[which(isQueryParameter(data$obj))]),
+                 as.character(data$pred[which(isQueryParameter(data$pred))])
+                 )
+    cst$parameters<-unique(c(cst$parameters,
+                             parameter))
+
+    object@constraint<-cst
+
+  }else{
+      print(paste("[addConstraintFrame] warning no constraint added"))
+    }
+    # Add check constraint
+  return(object)
+})
+
+
 isQueryParameter<-function(x){
   return(str_detect("^<<[a-zA-Z-_]*>>$",string = x))}
 isQueryVariable<-function(x){
@@ -86,11 +172,17 @@ isFixedValue<-function(x){
 
 
 #### BUILD Query #####
+#' @title build
+#' @description buold the corresponding query in the object (sparql_query_object)
+## @input param_data : data.frame that elicit element parameter in the query, it should be contain 2 columns : parameter, value and contain exactly 1 row by query parameter
+## @input keep : elements that should be return by the query, if keep is empty all query eement will be return (?XXX)
+#' @export
 setGeneric(name="build",
-           def=function(object){
+           def=function(object,param_data){
              standardGeneric("build")
            })
-setMethod(f="build",signature="SPARQL_query_builder",function(object){
+setMethod(f="build",signature="SPARQL_query_builder",function(object,param_data){
+
   pref_string<-paste(unlist(apply(X = getPrefix(object), 1,FUN = function(X){
       return(computePrefix(uri=X[2],prefix=X[1]))
   })),collapse = "")
@@ -98,13 +190,19 @@ setMethod(f="build",signature="SPARQL_query_builder",function(object){
     return(computeGraph(X))
   })),collapse=" ")
 
-  selection_string<-computeSelect(object@constraint[["element"]])
+  selection_string<-computeSelect(getConstraint(object)[["element"]])
 
-  bodyQuery_string<-computeBodyConstraint(object@constraint[["constraintDescription"]])
-  print(paste(c(pref_string, selection_string, from_string, bodyQuery_string),collapse=" \n"))
-  return(paste(c(pref_string, selection_string, from_string, bodyQuery_string),collapse=" \n"))
+  if(length(getParameterConstraint(object))==0){
+    bodyQuery_string<-computeBodyConstraint(getConstraint(object))
+  }else {
+    bodyQuery_string<-computeBodyConstraint(getConstraint(object),param_data )
+
+  }
+  return(paste(c(pref_string, selection_string, from_string, bodyQuery_string),collapse=" \n "))
 
 })
+
+
 
 computePrefix<-function(prefix, uri){
   return(paste("PREFIX",
@@ -129,11 +227,44 @@ computeSelect<-function(elements){
   }else{
     data <- paste(unique(elements), collapse = " ")
   }
+  print(data)
+
   return(paste("SELECT", data))
 }
 
 
-computeBodyConstraint<-function(constraint_data){
+computeBodyConstraint<-function(constraint_data, param_data=NULL){
+
+  #### envoie une liste maintenant
+  c_data<-constraint_data[["constraintDescription"]]
+  p_data<-constraint_data[["parameters"]]
+  data<-character(0)
+  #-- pas de contrainte
+  if(nrow(c_data) == 0){
+    data <-"?s ?p ?o"
+  }else{
+    data <- paste(unique(c_data$string), collapse=" . \n ")
+    if( length(p_data) >0 ){
+      if(! missing(param_data) && setequal(names(param_data),c("parameter","value"))
+         && length(setdiff(p_data, param_data$parameter))==0){
+        if(length(unique(param_data$parameter)) < length(param_data$parameter)){
+          warning("[build Body string] some parameter data have multiple value in parameter_data")
+        }
+        for(p in 1:nrow(param_data)){
+          data<-gsub(x=data, pattern = param_data$parameter[p],replacement = param_data$value[p])
+        }
+          print(data)
+      }else{
+        data<-character(0)
+        stop("[build Body string] missing parameters or unapporpriate parameter_data structure")
+      }
+    }
+    #-- contrainte existante
+  }
+  return(paste(c(" WHERE{",data,"}"),collaspe =" "))
+}
+
+computeBodyConstraint_old<-function(constraint_data){
   if(nrow(constraint_data) == 0){
     data <-"?s ?p ?o"
   }else{
@@ -141,7 +272,6 @@ computeBodyConstraint<-function(constraint_data){
   }
   return(paste(c(" WHERE{",data,"}"),collaspe =" "))
 }
-
 
 
 
--- SPARQLengine/R/sparql_function_jpr.R
+++ SPARQLengine/R/sparql_function_jpr.R
@@ -2,24 +2,41 @@ library("jsonlite")#check imports
 library("httr")
 library("data.table")
 
+
+
+
+#' @title sparqlGet
+#' sparql query using get http method
+#'
+#'@param url endpoint url
+#'@param sparql name of data frame on which foreign key constraint is checked
+#'@param type STATIC to csv value
+
+#'@return data.table results
+#'@import httr
+#'@import data.table
+#'
+#' @export
+#'
+sparqlGet <- function(url, sparql, type = "csv") {
+  url <- paste0(url, "/query")
+ r <- GET(url, query = list(query = sparql), accept("text/csv"))
+
+    csv <- httr::content(r, as = "text")
+  dt  <- fread(csv, header = T, sep = ",")
+  dtclean <- dt[ , lapply(.SD, function(x) gsub("http:\\/\\/[a-zA-Z0-9.\\/-]+[\\/#]{1}", "", x))]
+  return(dtclean)
+}
+
+###### UNUSED function
+
+
 sparqlPost <- function(url, sparql) {
   url <- paste0(url, "/update")
   r <- POST(url, body = sparql)
   # TODO check 204 status
   return(r)
 }
-
-
-sparqlGet <- function(url, sparql, type = "csv") {
-  url <- paste0(url, "/query")
-  r <- GET(url, query = list(query = sparql), accept("text/csv"))
-  csv <- httr::content(r, as = "text")
-  dt  <- fread(csv, header = T, sep = ",")
-  dtclean <- dt[ , lapply(.SD, function(x) gsub("http:\\/\\/[a-zA-Z0-9.\\/-]+[\\/#]{1}", "", x))]
-  return(dtclean)
-}
-
-###### TO check diff
 sparqlUpload <- function(url, file) {
   url <- paste0(url, "/data")
   r <- POST(url, body = upload_file(file, "text/turtle"))
